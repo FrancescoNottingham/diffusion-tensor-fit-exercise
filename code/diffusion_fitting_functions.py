b91ccc615a732c6f0fd0b0fc72d1ee09505bf2d8
Francesco D'Antonio <lpxfd2@mslaprad03.nottingham.ac.uk>
2023-10-15 17:12:55 +0100

    changed s0 so we are only passing througha single value instead of the 3D matrix"

--- code/diffusion_fitting_functions.py
+++ code/diffusion_fitting_functions.py
@@ -48,21 +48,22 @@ def create_matrix_s0(data,bvals):
     '''
     return np.mean(data[...,np.where(bvals==0)[0]],axis=data.ndim-1)
 
-def calculate_C_vals(data,bvals,x,y,z):
+def get_s0_value(s0,x,y,z):
+    return s0[x,y,z]
+
+def calculate_C_vals(data,s0_value,x,y,z):
     ''' Calculates C values from Stam's thesis paper (-ln(S_k/s0))
     '''
-
-    s0 = create_matrix_s0(data,bvals)
 
     C_vals = np.zeros(data.shape[-1])
     if s0[x,y,z] == 0:
         return C_vals
     else:
         for i in np.arange(0,data.shape[-1]):
-            C_vals[i] = -np.log(data[x,y,z,i]/s0[x,y,z])
+            C_vals[i] = -np.log(data[x,y,z,i]/s0_value)
         return C_vals
     
-def calculate_diffusion_values(data,bvals,bvecs,x,y,z):
+def calculate_diffusion_values(data,s0_value,bvals,bvecs,x,y,z):
     ''' Returns the diffusion tensor values (X) in the following order:
     [Dxx,Dyy,Dzz,Dxy,Dyz,Dxz]
 
@@ -72,7 +73,7 @@ def calculate_diffusion_values(data,bvals,bvecs,x,y,z):
     '''
 
     A = create_matrix_A(bvals,bvecs)
-    C = calculate_C_vals(data,bvals,x,y,z)
+    C = calculate_C_vals(data,s0_value,x,y,z)
 
     AT_dot_A_inv = np.linalg.inv(np.dot(np.transpose(A),A))
     AT_dot_C = np.dot(np.transpose(A),C)
@@ -113,13 +114,16 @@ def diffusion_tensor_fit(diffusion_data,brain_mask,bvals,bvecs):
 
     fractional_anisotropy = np.zeros(diffusion_data.shape)
 
+    s0 = create_matrix_s0(diffusion_data,bvals)
+    
     for x in np.arange(diffusion_data.shape[0]):
         for y in np.arange(diffusion_data.shape[1]):
             for z in np.arange(diffusion_data.shape[2]):
                 if brain_mask[x,y,z] == 0 or np.all(diffusion_data[x,y,z,:])==0:
                     fractional_anisotropy[x,y,z] = 0
                 else:
-                    diffusion_tensor_values = calculate_diffusion_values(diffusion_data,bvals,bvecs,x,y,z)
+                    s0_value = get_s0_value(s0,x,y,z)
+                    diffusion_tensor_values = calculate_diffusion_values(diffusion_data,s0_value,bvals,bvecs,x,y,z)
                     evals, evecs = calculate_diffusion_eigenvalues(diffusion_tensor_values)
                     fractional_anisotropy[x,y,z] = calculate_fractional_anisotropy(evals)
 
