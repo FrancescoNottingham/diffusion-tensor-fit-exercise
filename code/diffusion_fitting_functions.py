978815238b0d0df63aad776785b4a591c13c53d9
Francesco D'Antonio <lpxfd2@mslaprad03.nottingham.ac.uk>
2023-10-16 12:24:24 +0100

    Added functions to remove 0 values, run time 15 secs. Added tests to check the functions work

--- code/diffusion_fitting_functions.py
+++ code/diffusion_fitting_functions.py
@@ -26,14 +26,10 @@ def create_bvec_array(bvecs):
     The function iterates over all bvecs to give a matrix which is 6 colums and N rows (number
     of bvecs - the column number)
     '''
+        
+    G_values = np.array([bvecs[0,:]**2,bvecs[1,:]**2,bvecs[2,:]**2, 2*bvecs[1,:]*bvecs[0,:], 2*bvecs[2,:]*bvecs[1,:], 2*bvecs[0,:]*bvecs[2,:]])
 
-    G_values = np.zeros((bvecs.shape[1],6))
-    
-    for i in np.arange(bvecs.shape[1]):
-        G1 = np.tile(bvecs[:,i], (1,2))
-        G2 = np.append(bvecs[:,i], [2*bvecs[1,i], 2*bvecs[2,i], 2*bvecs[0,i]])
-        G_values[i,:] = G1*G2
-    return G_values
+    return np.transpose(G_values)
 
 def create_matrix_A(bvals,bvecs):
     ''' Creates the A matrix in equation 2.11 of Stam's thesis
@@ -41,6 +37,7 @@ def create_matrix_A(bvals,bvecs):
 
     G_matrix = create_bvec_array(bvecs)
     b_matrix = create_bval_array(bvals)
+
     return G_matrix*b_matrix
 
 def create_matrix_s0(data,bvals):
@@ -48,22 +45,35 @@ def create_matrix_s0(data,bvals):
     '''
     return np.mean(data[...,np.where(bvals==0)[0]],axis=data.ndim-1)
 
+
 def get_s0_value(s0,x,y,z):
     return s0[x,y,z]
 
-def calculate_C_vals(data,s0_value,x,y,z):
+def remove_b0s(data,bvals,bvecs):
+
+    index = np.nonzero(bvals)[0]
+    new_data = data[...,index]
+    new_bvals = bvals[index]
+    new_bvecs = bvecs[:,index]
+    
+    return new_data, new_bvals, new_bvecs
+
+def extract_non0_data(data, bvals, bvecs, x,y,z):
+
+    timeseries_all = data[x,y,z,:]
+    index = np.nonzero(timeseries_all)[0]
+    timeseries = data[x,y,z,index]
+    new_bvals = bvals[index]
+    new_bvecs = bvecs[:,index]
+
+    return timeseries, new_bvals, new_bvecs
+
+def calculate_C_vals(timeseries,s0_value):
     ''' Calculates C values from Stam's thesis paper (-ln(S_k/s0))
     '''
-
-    C_vals = np.zeros(data.shape[-1])
-    if s0_value == 0:
-        return C_vals
-    else:
-        for i in np.arange(0,data.shape[-1]):
-            C_vals[i] = -np.log(data[x,y,z,i]/s0_value)
-        return C_vals
+    return -np.log(timeseries/s0_value)
     
-def calculate_diffusion_values(data,s0_value,bvals,bvecs,x,y,z):
+def calculate_diffusion_values(timeseries,s0_value,bvals,bvecs):
     ''' Returns the diffusion tensor values (X) in the following order:
     [Dxx,Dyy,Dzz,Dxy,Dyz,Dxz]
 
@@ -73,10 +83,10 @@ def calculate_diffusion_values(data,s0_value,bvals,bvecs,x,y,z):
     '''
 
     A = create_matrix_A(bvals,bvecs)
-    C = calculate_C_vals(data,s0_value,x,y,z)
+    C = calculate_C_vals(timeseries,s0_value)
 
     AT_dot_A_inv = np.linalg.inv(np.dot(np.transpose(A),A))
-    AT_dot_C = np.dot(np.transpose(A),C)
+    AT_dot_C = np.transpose(np.dot(C,A))
     return np.dot(AT_dot_A_inv,AT_dot_C)
 
 def calculate_diffusion_eigenvalues(X):
@@ -97,9 +107,8 @@ def calculate_diffusion_eigenvalues(X):
     diff_tensor[0,2] = X[5] 
     diff_tensor[2,0] = X[5]
 
-    eval, evec = np.linalg.eig(diff_tensor)
 
-    return eval, evec
+    return np.linalg.eigvalsh(diff_tensor)
 
 def calculate_fractional_anisotropy(evals):
     ''' Calculates fractional anisotropy given the eigenvalues of a diffusion tensor
@@ -110,36 +119,28 @@ def calculate_fractional_anisotropy(evals):
 
     return np.sqrt(1-(l_1*l_2 + l_2*l_3 + l_3*l_1)/(l_1**2 + l_2**2 + l_3**2))
 
-def remove_b0s(data,bvals,bvecs):
-
-    index = np.nonzero(bvals)[0]
-    data = data[...,index]
-    bvals = bvals[index]
-    bvecs = bvecs[...,index]
-    
-    return data, bvals, bvecs
-
 def diffusion_tensor_fit(diffusion_data,brain_mask,bvals,bvecs):
 
-    fractional_anisotropy = np.zeros(diffusion_data.shape)
+    fractional_anisotropy = np.zeros(diffusion_data.shape[:-1])
 
     s0 = create_matrix_s0(diffusion_data,bvals)
-    
-    diffusion_data, bvals, bvecs = remove_b0s(diffusion_data,bvals,bvecs)
 
-    for x in np.arange(diffusion_data.shape[0]):
-        for y in np.arange(diffusion_data.shape[1]):
-            for z in np.arange(diffusion_data.shape[2]):
+    diffusion_data_no_b0, bvals_no_b0, bvecs_no_b0 = remove_b0s(diffusion_data,bvals,bvecs)
 
-                if brain_mask[x,y,z] == 0 or np.all(diffusion_data[x,y,z,:])==0:
+    for x in np.arange(diffusion_data_no_b0.shape[0]):
+        for y in np.arange(diffusion_data_no_b0.shape[1]):
+            for z in np.arange(diffusion_data_no_b0.shape[2]):
+
+                if brain_mask[x,y,z] == 0:
                     fractional_anisotropy[x,y,z] = 0
                 else:
                     s0_value = get_s0_value(s0,x,y,z)
-                    diffusion_tensor_values = calculate_diffusion_values(diffusion_data,s0_value,bvals,bvecs,x,y,z)
-                    evals, evecs = calculate_diffusion_eigenvalues(diffusion_tensor_values)
-                    fractional_anisotropy[x,y,z] = calculate_fractional_anisotropy(evals)
+                    timeseries_no0, bvals_no0, bvecs_no0 = extract_non0_data(diffusion_data_no_b0,bvals_no_b0,bvecs_no_b0, x,y,z)
+                    if timeseries_no0.size < 7:
+                        fractional_anisotropy[x,y,z] = 0
+                    else:
+                        diffusion_tensor_values = calculate_diffusion_values(timeseries_no0,s0_value,bvals_no0,bvecs_no0)
+                        evals = calculate_diffusion_eigenvalues(diffusion_tensor_values)
+                        fractional_anisotropy[x,y,z] = calculate_fractional_anisotropy(evals)
 
     return fractional_anisotropy
-
-
-    
--- code/functions_test.py
+++ code/functions_test.py
@@ -29,9 +29,37 @@ def test_create_matrix_A():
                                [18,8,2,24,8,12]])
     
     assert (test_solution3 == create_matrix_A(test_array1,test_array2)).all
+    assert (test_solution3.shape == create_matrix_A(test_array1,test_array2).shape)
 
 def test_create_matrix_s0():
     bval_test = np.array([0,1,0,2,3,0,0])
     data_test = np.array([4,0,4,0,0,4,4])
 
     assert create_matrix_s0(data_test,bval_test) == 4
+
+def test_remove_b0s():
+    
+    data = np.array([[5,1,3],[4,2,2],[2,3,1]])
+    bvals = np.array([1,0,3])
+    bvecs = np.array([[0,1,3],[0,2,2],[0,3,1]])
+
+    data, bvals, bvecs = remove_b0s(data,bvals,bvecs)
+
+    assert (data == np.array([[5,3],[4,2],[2,1]])).all
+    assert (bvals == np.array([1,3])).all
+    assert (bvecs == np.array([[0,3],[0,2],[0,1]])).all
+
+def test_extract_non0_data():
+    x = 0
+    y = 0
+    z = 0
+    data = np.array([[[[1,1],[1,1]],[[1,1],[1,1]]],
+                     [[[0,0],[0,0]],[[0,0],[0,0]]]])
+    bvals = np.array([2,7])
+    bvecs = np.array([[5,3],[3,2],[7,1]])
+
+    timeseries, bvals, bvecs = extract_non0_data(data, bvals, bvecs, x,y,z)
+
+    assert (timeseries == np.array([1])).all
+    assert (bvals == np.array([2])).all
+    assert (bvecs == np.array([[5],[3],[7]])).all
